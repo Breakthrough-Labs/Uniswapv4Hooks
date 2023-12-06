71d301dd40c27f44fe3de8950480ca18eeab69c6
Conor Gallagher <dontlisten33@gmail.com>
2023-12-06 12:13:13 -0800

    updated file structure and Remix README.MD files added

--- src/FAIR_TRADE/utils/PoolModifyPositionTest.sol
+++ src/FAIR_TRADE/utils/PoolModifyPositionTest.sol
@@ -1,18 +1,3 @@
-/*
-
- ██████  ██████   ██████  ██   ██ ██████   ██████   ██████  ██   ██    ██████  ███████ ██    ██
-██      ██    ██ ██    ██ ██  ██  ██   ██ ██    ██ ██    ██ ██  ██     ██   ██ ██      ██    ██
-██      ██    ██ ██    ██ █████   ██████  ██    ██ ██    ██ █████      ██   ██ █████   ██    ██
-██      ██    ██ ██    ██ ██  ██  ██   ██ ██    ██ ██    ██ ██  ██     ██   ██ ██       ██  ██
- ██████  ██████   ██████  ██   ██ ██████   ██████   ██████  ██   ██ ██ ██████  ███████   ████
-
-Find any smart contract, and build your project faster: https://www.cookbook.dev
-Twitter: https://twitter.com/cookbook_dev
-Discord: https://discord.gg/WzsfPcfHrk
-
-Find this contract on Cookbook: https://www.cookbook.dev/contracts/undefined?utm=code
-*/
-
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
